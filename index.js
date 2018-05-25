2ed73229303f130f1023ee495b536da24383f9d4
Jess Telford <jess+github@jes.st>
2018-05-25 14:49:41 +1000

    Preparing for AND/OR operators

--- index.js
+++ index.js
@@ -26,34 +26,40 @@ const postSchema = new Schema(
   { timestamps: true }
 );
 
-function getRelModel(model, path) {
+// Non-recursive, should be called on every level
+function makeANDexplicit(args) {
+  if (Array.isArray(args)) {
+    return { AND: args };
+  }
+
+  return args;
 }
 
-postSchema.statics.lookup = function({ path, query, modifiers = {} }) {
+// NOTE: Doesn't check for implicit AND
+function invariantANDOR(args) {
+  if (args.AND) {
+    // AND
+    if (args.OR) {
+      throw new Error('Cannot combine AND + OR on same level:', args);
+    }
+  } else if (args.OR) {
+    if (args.AND) {
+      throw new Error('Cannot combine OR + AND on same level:', args);
+    }
+  }
+}
+
+function expressionPipeline({ path, query, modifiers = {} }, { model, joinPathName }) {
+
   let rel;
-  const many = this.schema.path(path).$isMongooseArray;
+  const many = model.schema.path(path).$isMongooseArray;
   if (many) {
-    rel = mongoose.model(this.schema.path(path).caster.options.ref);
+    rel = mongoose.model(model.schema.path(path).caster.options.ref);
   } else {
-    rel = mongoose.model(this.schema.path(path).options.ref);
+    rel = mongoose.model(model.schema.path(path).options.ref);
   }
 
-  // MongoDB 3.6 and up $lookup with sub-pipeline
-  // FIXME: Not working
-  console.log('rel.collection.name', rel.collection.name);
-
-  if (modifiers.some) {
-    // $expr: { $in: ["$_id", `$$${path}`] }
-  } else if (modifiers.every) {
-    // ??
-    // 
-  }
-
-  // If there are more than one path/query combo, we can do them with `$facet`
-  // aggregate state
-
-  const joinPathName = `${path}__matched`;
-  return this.aggregate([
+  return [
     {
       // JOIN
       $lookup: {
@@ -80,49 +86,98 @@ postSchema.statics.lookup = function({ path, query, modifiers = {} }) {
     // document's size. In the case of one-to-one, we'd have to check that the
     // resulting size is exactly 1 (which is the same as `$ne: []`?)
     { $match: { [joinPathName]: { $exists: true, $ne: [] } } },
+  ];
+}
+
+function postAggregateMutationFactory({ path }, { model, joinPathName }) {
+
+  let rel;
+  const many = model.schema.path(path).$isMongooseArray;
+  if (many) {
+    rel = mongoose.model(model.schema.path(path).caster.options.ref);
+  } else {
+    rel = mongoose.model(model.schema.path(path).options.ref);
+  }
+
+  // Recreate Mongoose instances of the sub items every time to allow for
+  // further operations to be performed on those sub items via Mongoose.
+  return item => {
+    let joinedItems;
+
+    if (many) {
+      joinedItems = item[path].map((itemId, itemIndex) => {
+        const joinedItem = item[joinPathName][itemIndex];
+        if (joinedItem) {
+          // I'm pretty certain that the two arrays should have the same
+          // order, but not 100%, so am doing a sanity check here so we can
+          // write a less efficient, but more accurate algorithm if needed
+          if (!itemId.equals(joinedItem._id)) {
+            throw new Error('Expected results from MongoDB aggregation to be ordered, but IDs are different.');
+          }
+          return rel(item[joinPathName][itemIndex]);
+        }
+        return itemId;
+      });
+    } else {
+      const joinedItem = item[joinPathName][0];
+      if (!joinedItem || !joinedItem._id.equals(item[path])) {
+        // Shouldn't be possible due to the { $exists: true, $ne: [] }
+        // aggregation step above, but in case that fails or doesn't behave
+        // as expected, we can catch that now.
+        throw new Error('Expected MongoDB aggregation to correctly filter to a single related item, but no item found.');
+      }
+      joinedItems = rel(joinedItem);
+    }
+
+    const newItemValues = {
+      ...item,
+      [path]: joinedItems,
+    };
+
+    // Get rid of the temporary data key we used to join on
+    delete newItemValues[joinPathName];
+
+    return model(newItemValues);
+  };
+}
+
+postSchema.statics.lookup = function(args) {
+
+  /*
+  if (modifiers.some) {
+    // $expr: { $in: ["$_id", `$$${path}`] }
+  } else if (modifiers.every) {
+    // ??
+    // 
+  }
+  */
+
+  args = makeANDexplicit(args);
+  invariantANDOR(args);
+
+  let pipeline;
+  let postAggregateMutation;
+
+  // TODO
+  if (args.AND) {
+    throw new Error('AND not implemented');
+    // If there are more than one path/query combo, we can do them with `$facet`
+    // aggregate state
+  } else if (args.OR) {
+    throw new Error('OR not implemented');
+  } else {
+    // An expression
+    const joinPathName = `${args.path}__matched`;
+    pipeline = expressionPipeline(args, { joinPathName, model: this });
+    postAggregateMutation = postAggregateMutationFactory(args, { joinPathName, model: this });
+  }
+
+  return this.aggregate([
+    ...pipeline,
   ])
     .exec()
     .then(data => (
-      // Recreate Mongoose instances of the sub items every time to allow for
-      // further operations to be performed on those sub items via Mongoose.
-      data.map(item => {
-        let joinedItems;
-
-        if (many) {
-          joinedItems = item[path].map((itemId, itemIndex) => {
-            const joinedItem = item[joinPathName][itemIndex];
-            if (joinedItem) {
-              // I'm pretty certain that the two arrays should have the same
-              // order, but not 100%, so am doing a sanity check here so we can
-              // write a less efficient, but more accurate algorithm if needed
-              if (!itemId.equals(joinedItem._id)) {
-                throw new Error('Expected results from MongoDB aggregation to be ordered, but IDs are different.');
-              }
-              return rel(item[joinPathName][itemIndex]);
-            }
-            return itemId;
-          });
-        } else {
-          const joinedItem = item[joinPathName][0];
-          if (!joinedItem || !joinedItem._id.equals(item[path])) {
-            // Shouldn't be possible due to the { $exists: true, $ne: [] }
-            // aggregation step above, but in case that fails or doesn't behave
-            // as expected, we can catch that now.
-            throw new Error('Expected MongoDB aggregation to correctly filter to a single related item, but no item found.');
-          }
-          joinedItems = rel(joinedItem);
-        }
-
-        const newItemValues = {
-          ...item,
-          [path]: joinedItems,
-        };
-
-        // Get rid of the temporary data key we used to join on
-        delete newItemValues[joinPathName];
-
-        return this(newItemValues);
-      })
+      data.map(postAggregateMutation)
     )).then(data => {
       console.log(util.inspect(data, { depth: null, colors: true }));
       return data;
@@ -173,21 +228,39 @@ const Post = mongoose.model("Post", postSchema);
       categories: [categories[2], categories[3]],
     });
 
-    // Query with our static
-    const postAuthorNames = [/Jess/i];
-    let result = await Post.lookup({
-      path: "author",
-      query: { "name": { $in: postAuthorNames } }
-    });
-    console.log(`Lookup posts with Author ${postAuthorNames}`);
+    await (async () => {
+      const postAuthorNames = [/Jess/i];
+      console.log(`Lookup posts with Author ${postAuthorNames}`);
+      await Post.lookup({
+        path: "author",
+        query: { "name": { $in: postAuthorNames } }
+      });
+    })();
 
-    // Query with our static
-    const postCategoryNames = ['React', 'GraphQL'];
-    result = await Post.lookup({
-      path: "categories",
-      query: { "name": { $in: postCategoryNames } }
-    });
-    console.log(`Lookup posts with Categories ${postCategoryNames}`);
+    await (async () => {
+      const postCategoryNames = ['React', 'GraphQL'];
+      console.log(`Lookup posts with Categories ${postCategoryNames}`);
+      await Post.lookup({
+        path: "categories",
+        query: { "name": { $in: postCategoryNames } }
+      });
+    })();
+
+    await (async () => {
+      const postAuthorNames = [/Jess/i];
+      const postCategoryNames = ['React', 'GraphQL'];
+      console.log(`Lookup posts with Categories ${postCategoryNames}, and Author ${postAuthorNames}`);
+      await Post.lookup([
+        {
+          path: "categories",
+          query: { "name": { $in: postCategoryNames } }
+        },
+        {
+          path: "author",
+          query: { "name": { $in: postAuthorNames } }
+        }
+      ]);
+    })();
 
     await mongoose.disconnect();
   } catch (e) {
